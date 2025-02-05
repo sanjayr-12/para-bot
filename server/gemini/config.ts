5c56607f4e04ad455d40bcb35750177e585216b4
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-02-05 23:26:42 +0530

    updated client and server files

--- server/gemini/config.ts
+++ server/gemini/config.ts
@@ -1,6 +1,9 @@
 import { GoogleGenerativeAI } from "npm:@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(Deno.env.get("GEM_API") || "");
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  systemInstruction: Deno.env.get("SYS_INS"),
+});
 
 export default model;
